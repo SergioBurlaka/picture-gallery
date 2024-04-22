31d178904a5b9e9823a14a0b240e795f830566d7
burlaka sergii <burlaka01@gmail.com>
2024-04-22 23:16:08 +0300

    feat: build improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,17 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const TopNav = () => {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <h2>Gallery</h2>
+      <div>
+        Sign in
+      </div>
+    </nav>
+  );
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -20,7 +31,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable} flex flex-col gap-4 `}>
+        <TopNav />
+
+        {children}
+      </body>
     </html>
   );
 }
