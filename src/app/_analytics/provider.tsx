e36ab30352508af88b5cb106d2f537e494ee958d
burlaka sergii <burlaka01@gmail.com>
2024-05-04 18:38:42 +0300

    feat: add post hog analitics

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -1,12 +1,37 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!,{
+    api_host: "/ingest",
+    ui_host: 'https://us.i.posthog.com' // or 'https://eu.i.posthog.com' if your PostHog is hosted in Europe
   });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  return (
+    <PostHogProvider client={posthog}>
+      <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+    </PostHogProvider>
+  );
 }
+
+const PostHogAuthWrapper = ({ children }: { children: React.ReactNode }) => {
+  const auth = useAuth();
+  const userInfo = useUser();
+
+  useEffect(() => {
+    if (userInfo.user) {
+      posthog.identify(userInfo.user.id, {
+        email: userInfo.user.emailAddresses[0]?.emailAddress,
+        name: userInfo.user.fullName,
+      });
+    } else if (!auth.isSignedIn) {
+      posthog.reset();
+    }
+  }, [auth, userInfo]);
+
+  return children;
+};
